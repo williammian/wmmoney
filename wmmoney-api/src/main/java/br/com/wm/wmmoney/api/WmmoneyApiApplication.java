0854807e67975b0008ffa75d87e96d5afd7a5d58
William Mian <william_mian@yahoo.com.br>
2019-08-20 15:32:49 -0300

    criando projeto da API

---
+++ wmmoney-api/src/main/java/br/com/wm/wmmoney/api/WmmoneyApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.wm.wmmoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmmoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WmmoneyApiApplication.class, args); 
+	}
+
+}
