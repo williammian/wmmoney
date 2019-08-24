46d524265c4fb114d93339a42e2a8d28de9e828f
williammian <william_mian@yahoo.com.br>
2019-08-24 14:21:51 -0300

    Retornando 404 caso não exista a categoria

--- wmmoney-api/src/main/java/br/com/wm/wmmoney/api/resource/CategoriaResource.java
+++ wmmoney-api/src/main/java/br/com/wm/wmmoney/api/resource/CategoriaResource.java
@@ -42,8 +42,9 @@ public class CategoriaResource {
 	}
 	
 	@GetMapping("/{codigo}")
-	public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
-		return categoriaRepository.findOne(codigo);
+	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+		Categoria categoria = categoriaRepository.findOne(codigo);
+		return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
 	}
 
 }
