6354f50a660110f9a739242bb74cb213fc28ed45
williammian <william_mian@yahoo.com.br>
2019-09-16 06:43:28 -0300

    Habilitando CORS globalmente desde que nao usado OAuth2

--- wmmoney-api/src/main/java/br/com/wm/wmmoney/api/WmmoneyApiApplication.java
+++ wmmoney-api/src/main/java/br/com/wm/wmmoney/api/WmmoneyApiApplication.java
@@ -2,6 +2,10 @@ package br.com.wm.wmmoney.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class WmmoneyApiApplication {
@@ -9,5 +13,16 @@ public class WmmoneyApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WmmoneyApiApplication.class, args); 
 	}
+	
+//	Habilita CORS globalmente desde que nao usado OAuth2
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurerAdapter() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/*").allowedOrigins("http://localhost:8080");
+//			}
+//		};
+//	}
 
 }
