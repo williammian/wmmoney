1a12e7c4cbba7b49f04b1b5f939089ffcaf14c50
William Mian <william_mian@yahoo.com.br>
2019-10-30 09:42:07 -0300

    criando formumlario de pesquisa de lancamentos

--- wmmoney-ui/src/app/app.module.ts
+++ wmmoney-ui/src/app/app.module.ts
@@ -1,7 +1,9 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { TabViewModule } from 'primeng/primeng';
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import { ButtonModule } from 'primeng/components/button/button';
 
 import { AppComponent } from './app.component';
 
@@ -10,8 +12,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    TabViewModule
+    InputTextModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
