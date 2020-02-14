68d0838d986e8f586e35b763cf82d067d26f807a
williammian <william_mian@yahoo.com.br>
2020-02-14 07:48:53 -0200

    configurando a paginacao lazy do primeng

--- wmmoney-ui/src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ wmmoney-ui/src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
+import { LazyLoadEvent } from 'primeng/components/common/lazyloadevent';
+
 import { LancamentoService, LancamentoFiltro } from './../lancamento.service';
 
 @Component({
@@ -8,20 +11,28 @@ import { LancamentoService, LancamentoFiltro } from './../lancamento.service';
 })
 export class LancamentosPesquisaComponent implements OnInit {
 
+  totalRegistros = 0;
   filtro = new LancamentoFiltro();
   lancamentos = [];
 
   constructor(private lancamentoService: LancamentoService) { }
 
   ngOnInit() {
-    this.pesquisar();
   }
 
-  pesquisar() {
+  pesquisar(pagina = 0) {
+    this.filtro.pagina = pagina;
+
     this.lancamentoService.pesquisar(this.filtro)
       .then(resultado => {
+        this.totalRegistros = resultado.total;
         this.lancamentos = resultado.lancamentos;
       });
+  }
+
+  aoMudarPagina(event: LazyLoadEvent) {
+    const pagina = event.first / event.rows;
+    this.pesquisar(pagina);
   }
 
 }
