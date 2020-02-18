1e2b50c97da62bea5ebc8f05011ffd16e3f4a6ac
William Mian <william_mian@yahoo.com.br>
2020-02-18 17:15:00 -0300

    mensagem de erro de usuario na exclusao de pessoa - errorResponse

--- wmmoney-ui/src/app/core/error-handler.service.ts
+++ wmmoney-ui/src/app/core/error-handler.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Response } from '@angular/http';
 
 import { ToastyService } from 'ng2-toasty';
 
@@ -12,6 +13,20 @@ export class ErrorHandlerService {
 
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
+
+    } else if (errorResponse instanceof Response
+        && errorResponse.status >= 400 && errorResponse.status <= 499) {
+      let errors;
+      msg = 'Ocorreu um erro ao processar a sua solicitação';
+
+      try {
+        errors = errorResponse.json();
+
+        msg = errors[0].mensagemUsuario;
+      } catch (e) { }
+
+      console.error('Ocorreu um erro', errorResponse);
+
     } else {
       msg = 'Erro ao processar serviço remoto. Tente novamente.';
       console.error('Ocorreu um erro', errorResponse);
