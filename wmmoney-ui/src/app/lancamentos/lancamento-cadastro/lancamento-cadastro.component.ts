f8f458cac7e458779c97c4a7b7980e854f97dbbf
williammian <william_mian@yahoo.com.br>
2020-02-22 15:23:49 -0300

    Preenchendo os campos na edicao de lancamentos

--- wmmoney-ui/src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
+++ wmmoney-ui/src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -36,10 +36,26 @@ export class LancamentoCadastroComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    console.log(this.route.snapshot.params['codigo']);
+    const codigoLancamento = this.route.snapshot.params['codigo'];
+
+    if (codigoLancamento) {
+      this.carregarLancamento(codigoLancamento);
+    }
 
     this.carregarCategorias();
     this.carregarPessoas();
+  }
+
+  get editando() {
+    return Boolean(this.lancamento.codigo)
+  }
+
+  carregarLancamento(codigo: number) {
+    this.lancamentoService.buscarPorCodigo(codigo)
+      .then(lancamento => {
+        this.lancamento = lancamento;
+      })
+      .catch(erro => this.errorHandler.handle(erro));
   }
 
   salvar(form: FormControl) {
