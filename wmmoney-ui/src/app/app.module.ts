537ac9bf6b12753742cd31a76b134e11c5c691f2
williammian <william_mian@yahoo.com.br>
2020-02-22 14:28:10 -0300

    Recebendo parametros da rota

--- wmmoney-ui/src/app/app.module.ts
+++ wmmoney-ui/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa
 const routes: Routes = [
   { path: 'lancamentos', component: LancamentosPesquisaComponent },
   { path: 'lancamentos/novo', component: LancamentoCadastroComponent },
+  { path: 'lancamentos/:codigo', component: LancamentoCadastroComponent },
   { path: 'pessoas', component: PessoasPesquisaComponent }
 ];
 
--- wmmoney-ui/src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
+++ wmmoney-ui/src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -1,5 +1,6 @@
 import { FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { ToastyService } from 'ng2-toasty';
 
@@ -30,10 +31,13 @@ export class LancamentoCadastroComponent implements OnInit {
     private pessoaService: PessoaService,
     private lancamentoService: LancamentoService,
     private toasty: ToastyService,
-    private errorHandler: ErrorHandlerService
+    private errorHandler: ErrorHandlerService,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
+    console.log(this.route.snapshot.params['codigo']);
+
     this.carregarCategorias();
     this.carregarPessoas();
   }
