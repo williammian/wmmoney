15143d971b0e5881d0463719fc460c061201bdf2
williammian <william_mian@yahoo.com.br>
2020-02-22 15:53:41 -0300

    salvando lancamentos editados

--- wmmoney-ui/src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
+++ wmmoney-ui/src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -59,12 +59,30 @@ export class LancamentoCadastroComponent implements OnInit {
   }
 
   salvar(form: FormControl) {
+    if (this.editando) {
+      this.atualizarLancamento(form);
+    } else {
+      this.adicionarLancamento(form);
+    }
+  }
+
+  adicionarLancamento(form: FormControl) {
     this.lancamentoService.adicionar(this.lancamento)
       .then(() => {
         this.toasty.success('Lançamento adicionado com sucesso!');
 
         form.reset();
         this.lancamento = new Lancamento();
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  atualizarLancamento(form: FormControl) {
+    this.lancamentoService.atualizar(this.lancamento)
+      .then(lancamento => {
+        this.lancamento = lancamento;
+
+        this.toasty.success('Lançamento alterado com sucesso!');
       })
       .catch(erro => this.errorHandler.handle(erro));
   }
