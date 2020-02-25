bff38d8c54404d581a8a0635b10d934637ecb6b0
williammian <william_mian@yahoo.com.br>
2020-02-25 17:19:03 -0300

    Exibindo o nome do usuario logado

--- wmmoney-ui/src/app/core/navbar/navbar.component.ts
+++ wmmoney-ui/src/app/core/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from 'app/seguranca/auth.service';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
   }
