d6deeb3760d6eeb82bf55c69da0ee8d80687b686
williammian <william_mian@yahoo.com.br>
2020-02-25 16:32:13 -0300

    Tratando casos de erros e sucesso de autenticacao

--- wmmoney-ui/src/app/seguranca/login-form/login-form.component.ts
+++ wmmoney-ui/src/app/seguranca/login-form/login-form.component.ts
@@ -1,5 +1,7 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
+import { ErrorHandlerService } from './../../core/error-handler.service';
 import { AuthService } from './../auth.service';
 
 @Component({
@@ -9,10 +11,20 @@ import { AuthService } from './../auth.service';
 })
 export class LoginFormComponent {
 
-  constructor(private auth: AuthService) { }
+  constructor(
+    private auth: AuthService,
+    private errorHandler: ErrorHandlerService,
+    private router: Router
+    ) { }
 
   login(usuario: string, senha: string) {
-    this.auth.login(usuario, senha);
+    this.auth.login(usuario, senha)
+      .then(() => {
+        this.router.navigate(['/lancamentos']);
+      })
+      .catch(erro => {
+        this.errorHandler.handle(erro);
+      });
   }
 
 }
