ef98325003e2a9165893d1c0c72ca9e479baf70e
William Mian <william_mian@yahoo.com.br>
2020-02-28 12:43:43 -0300

    Tratando acessos de usuarios deslogados na AuthGuard

--- wmmoney-ui/src/app/seguranca/auth.guard.ts
+++ wmmoney-ui/src/app/seguranca/auth.guard.ts
@@ -18,7 +18,19 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
 
-    if (next.data.roles && !this.auth.temQualquerPermissao(next.data.roles)) {
+    if (this.auth.isAccessTokenInvalido()) {
+      console.log('Navegação com access token inválido. Obtendo novo token...');
+
+      return this.auth.obterNovoAccessToken()
+        .then(() => {
+          if (this.auth.isAccessTokenInvalido()) {
+            this.router.navigate(['/login']);
+            return false;
+          }
+
+          return true;
+        });
+    } else if (next.data.roles && !this.auth.temQualquerPermissao(next.data.roles)) {
       this.router.navigate(['/nao-autorizado']);
       return false;
     }
