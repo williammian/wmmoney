62bb298deee6d5a6e4aa3d7bdafc713c7aebe32e
williammian <william_mian@yahoo.com.br>
2020-03-06 06:53:59 -0300

    Alterando de datatable para turbotable

--- wmmoney-ui/src/app/lancamentos/lancamentos.module.ts
+++ wmmoney-ui/src/app/lancamentos/lancamentos.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
 import { DataTableModule } from 'primeng/datatable';
 import { TooltipModule } from 'primeng/tooltip';
 import { InputTextareaModule } from 'primeng/inputtextarea';
@@ -26,6 +27,7 @@ import { LancamentosRoutingModule } from './lancamentos-routing.module';
 
     InputTextModule,
     ButtonModule,
+    TableModule,
     DataTableModule,
     TooltipModule,
     InputTextareaModule,
