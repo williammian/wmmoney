a757d6d8a12b3ea7dcaa0e82a1284ed8ba774256
William Mian <william_mian@yahoo.com.br>
2020-03-09 13:32:52 -0300

    Plotando graficos com dados estaticos

--- wmmoney-ui/src/app/dashboard/dashboard.module.ts
+++ wmmoney-ui/src/app/dashboard/dashboard.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PanelModule } from 'primeng/panel';
+import { ChartModule } from 'primeng/chart';
+
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SharedModule } from 'app/shared/shared.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   imports: [
     CommonModule,
 
+    PanelModule,
+    ChartModule,
+
     SharedModule,
     DashboardRoutingModule
   ],
-  declarations: []
+  declarations: [DashboardComponent]
 })
 export class DashboardModule { }
