7ea4a102611a8babca1c3789348f5e8b56259809
William Mian <william_mian@yahoo.com.br>
2020-03-09 12:47:33 -0300

    correcao dashboard module

--- wmmoney-ui/src/app/dashboard/dashboard.module.ts
+++ wmmoney-ui/src/app/dashboard/dashboard.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { SharedModule } from 'app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
+
+    SharedModule,
     DashboardRoutingModule
   ],
-  declarations: [DashboardComponent]
+  declarations: []
 })
 export class DashboardModule { }
