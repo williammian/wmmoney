0c2b9986311bf3b3b01502d99638bc6fdc59bb86
William Mian <william_mian@yahoo.com.br>
2020-03-10 12:50:24 -0300

    Buscando dados do grafico de pizza

--- wmmoney-ui/src/app/dashboard/dashboard/dashboard.component.ts
+++ wmmoney-ui/src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DashboardService } from '../dashboard.service';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,15 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  pieChartData = {
-    labels: ['Mensal', 'Educação', 'Lazer', 'Imprevistos'],
-    datasets: [
-      {
-        data: [2500, 2700, 550, 235],
-        backgroundColor: ['#FF9900', '#109618', '#990099', '#3B3EAC']
-      }
-    ]
-  };
+  pieChartData: any;
 
   lineChartData = {
     labels: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
@@ -32,9 +26,26 @@ export class DashboardComponent implements OnInit {
     ]
   };
 
-  constructor() { }
+  constructor(private dashboardService: DashboardService) { }
 
   ngOnInit() {
+    this.configurarGraficoPizza();
+  }
+
+  configurarGraficoPizza() {
+    this.dashboardService.lancamentosPorCategoria()
+      .then(dados => {
+        this.pieChartData = {
+          labels: dados.map(dado => dado.categoria.nome),
+          datasets: [
+            {
+              data: dados.map(dado => dado.total),
+              backgroundColor: ['#FF9900', '#109618', '#990099', '#3B3EAC', '#0099C6',
+                                  '#DD4477', '#3366CC', '#DC3912']
+            }
+          ]
+        };
+      });
   }
 
 }
