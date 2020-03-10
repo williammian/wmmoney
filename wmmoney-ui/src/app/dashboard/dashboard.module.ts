d05e243d97bb18f03e61d84d7a30eca8397ff2ad
William Mian <william_mian@yahoo.com.br>
2020-03-10 13:29:57 -0300

    Formatando labels no chartjs

--- wmmoney-ui/src/app/dashboard/dashboard.module.ts
+++ wmmoney-ui/src/app/dashboard/dashboard.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DecimalPipe } from '@angular/common';
 
 import { PanelModule } from 'primeng/panel';
 import { ChartModule } from 'primeng/chart';
@@ -18,6 +18,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     SharedModule,
     DashboardRoutingModule
   ],
-  declarations: [DashboardComponent]
+  declarations: [DashboardComponent],
+  providers: [DecimalPipe]
 })
 export class DashboardModule { }
--- wmmoney-ui/src/app/dashboard/dashboard/dashboard.component.ts
+++ wmmoney-ui/src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
 
 import { DashboardService } from '../dashboard.service';
 
@@ -10,10 +11,26 @@ import { DashboardService } from '../dashboard.service';
 export class DashboardComponent implements OnInit {
 
   pieChartData: any;
-
   lineChartData: any;
 
-  constructor(private dashboardService: DashboardService) { }
+  options = {
+    tooltips: {
+      callbacks: {
+        label: (tooltipItem, data) => {
+          const dataset = data.datasets[tooltipItem.datasetIndex];
+          const valor = dataset.data[tooltipItem.index];
+          const label = dataset.label ? (dataset.label + ': ') : '';
+
+          return label + this.decimalPipe.transform(valor, '1.2-2');
+        }
+      }
+    }
+  };
+
+  constructor(
+    private dashboardService: DashboardService,
+    private decimalPipe: DecimalPipe
+    ) { }
 
   ngOnInit() {
     this.configurarGraficoPizza();
