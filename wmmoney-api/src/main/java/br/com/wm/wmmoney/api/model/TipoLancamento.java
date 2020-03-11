1d5a26f1f8a39d1227e77945805f09abe096913a
William Mian <william_mian@yahoo.com.br>
2020-03-11 13:22:22 -0300

    Criando o DTO do relatorio lancamentos-por-pessoa

--- wmmoney-api/src/main/java/br/com/wm/wmmoney/api/model/TipoLancamento.java
+++ wmmoney-api/src/main/java/br/com/wm/wmmoney/api/model/TipoLancamento.java
@@ -2,7 +2,17 @@ package br.com.wm.wmmoney.api.model;
 
 public enum TipoLancamento {
 	
-	RECEITA,
-	DESPESA
+	RECEITA("Receita"),
+	DESPESA("Despesa");
+	
+	private final String descricao;
+	
+	TipoLancamento(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
 
 }
