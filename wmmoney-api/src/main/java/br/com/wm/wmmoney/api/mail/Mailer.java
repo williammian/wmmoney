8870f1aad8b2318ae62ac786cb33c6b76d3d0510
William Mian <william_mian@yahoo.com.br>
2020-03-17 12:50:47 -0300

    Processando o template com thymeleaf e enviando o e-mail

--- wmmoney-api/src/main/java/br/com/wm/wmmoney/api/mail/Mailer.java
+++ wmmoney-api/src/main/java/br/com/wm/wmmoney/api/mail/Mailer.java
@@ -1,23 +1,27 @@
 package br.com.wm.wmmoney.api.mail;
 
-//import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.context.event.ApplicationReadyEvent;
-//import org.springframework.context.event.EventListener;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Component
 public class Mailer {
 	
 	@Autowired
 	private JavaMailSender mailSender;
+	
+	@Autowired
+	private TemplateEngine thymeleaf;
 	
 //	@EventListener	//Evento que o spring lança quando a aplicação está pronta para ser utilizada
 //	private void teste(ApplicationReadyEvent event) {
@@ -26,6 +30,35 @@ public class Mailer {
 //				"Testando", "Olá!<br/>Teste ok.");
 //		System.out.println("Terminado o envio de e-mail...");
 //	}
+	
+//	@Autowired
+//	private LancamentoRepository repo;
+//	@EventListener
+//	private void teste2(ApplicationReadyEvent event) {
+//		String template = "mail/aviso-lancamentos-vencidos";
+//		
+//		List<Lancamento> lista = repo.findAll();
+//		
+//		Map<String, Object> variaveis = new HashMap<>();
+//		variaveis.put("lancamentos", lista);
+//		
+//		this.enviarEmail("williammian_mw@hotmail.com", 
+//				Arrays.asList("william_mian@yahoo.com.br"), 
+//				"Testando", template, variaveis);
+//		System.out.println("Terminado o envio de e-mail...");
+//	}
+	
+	public void enviarEmail(String remetente, 
+			List<String> destinatarios, String assunto, String template, 
+			Map<String, Object> variaveis) {
+		Context context = new Context(new Locale("pt", "BR"));
+		
+		variaveis.entrySet().forEach(e -> context.setVariable(e.getKey(), e.getValue()));
+		
+		String mensagem = thymeleaf.process(template, context);
+		
+		this.enviarEmail(remetente, destinatarios, assunto, mensagem);
+	}
 	
 	public void enviarEmail(String remetente, 
 			List<String> destinatarios, String assunto, String mensagem) {
--- wmmoney-api/src/main/java/br/com/wm/wmmoney/api/model/Lancamento.java
+++ wmmoney-api/src/main/java/br/com/wm/wmmoney/api/model/Lancamento.java
@@ -15,6 +15,8 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "lancamento")
 public class Lancamento {
@@ -51,6 +53,11 @@ public class Lancamento {
 	@ManyToOne
 	@JoinColumn(name = "codigo_pessoa")
 	private Pessoa pessoa;
+	
+	@JsonIgnore
+	public boolean isReceita() {
+		return TipoLancamento.RECEITA.equals(this.tipo);
+	}
 
 	public Long getCodigo() {
 		return codigo;
