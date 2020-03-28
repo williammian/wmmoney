c7aef273511c3522ecbe9f55abc7be44580405f6
williammian <william_mian@yahoo.com.br>
2020-03-28 16:37:04 -0300

    Editando contato

--- wmmoney-ui/src/app/pessoas/pessoa-cadastro/pessoa-cadastro.component.ts
+++ wmmoney-ui/src/app/pessoas/pessoa-cadastro/pessoa-cadastro.component.ts
@@ -19,6 +19,7 @@ export class PessoaCadastroComponent implements OnInit {
   pessoa = new Pessoa();
   exbindoFormularioContato = false;
   contato: Contato;
+  contatoIndex: number;
 
   constructor(
     private pessoaService: PessoaService,
@@ -42,10 +43,17 @@ export class PessoaCadastroComponent implements OnInit {
   prepararNovoContato() {
     this.exbindoFormularioContato = true;
     this.contato = new Contato();
+    this.contatoIndex = this.pessoa.contatos.length;
+  }
+
+  prepararEdicaoContato(contato: Contato, index: number) {
+    this.contato = this.clonarContato(contato);
+    this.exbindoFormularioContato = true;
+    this.contatoIndex = index;
   }
 
   confirmarContato(frm: FormControl) {
-    this.pessoa.contatos.push(this.clonarContato(this.contato));
+    this.pessoa.contatos[this.contatoIndex] = this.clonarContato(this.contato);
 
     this.exbindoFormularioContato = false;
 
