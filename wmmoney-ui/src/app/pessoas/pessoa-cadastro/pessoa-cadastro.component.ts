0fb20e7db8e3085cdaf9e588171ecb8e62dd4771
williammian <william_mian@yahoo.com.br>
2020-03-28 16:50:59 -0300

    Removendo contato

--- wmmoney-ui/src/app/pessoas/pessoa-cadastro/pessoa-cadastro.component.ts
+++ wmmoney-ui/src/app/pessoas/pessoa-cadastro/pessoa-cadastro.component.ts
@@ -60,6 +60,10 @@ export class PessoaCadastroComponent implements OnInit {
     frm.reset();
   }
 
+  removerContato(index: number) {
+    this.pessoa.contatos.splice(index, 1);
+  }
+
   clonarContato(contato: Contato): Contato {
     return new Contato(contato.codigo,
       contato.nome, contato.email, contato.telefone);
