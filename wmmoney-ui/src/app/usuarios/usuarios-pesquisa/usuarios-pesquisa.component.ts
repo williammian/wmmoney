dde2f2baf16e42addaf92a76258cddcfb5b03ad7
William Mian <william_mian@yahoo.com.br>
2020-04-06 12:59:30 -0300

    Componente pesquisa de usuarios

--- wmmoney-ui/src/app/usuarios/usuarios-pesquisa/usuarios-pesquisa.component.ts
+++ wmmoney-ui/src/app/usuarios/usuarios-pesquisa/usuarios-pesquisa.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { MessageService } from 'primeng/components/common/messageservice';
+import { ConfirmationService, LazyLoadEvent } from 'primeng/components/common/api';
+
+import { ErrorHandlerService } from 'app/core/error-handler.service';
+import { UsuarioFiltro, UsuarioService } from './../usuario.service';
 
 @Component({
   selector: 'app-usuarios-pesquisa',
@@ -7,9 +15,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuariosPesquisaComponent implements OnInit {
 
-  constructor() { }
+  totalRegistros = 0;
+  filtro = new UsuarioFiltro();
+  usuarios = [];
+  @ViewChild('tabela') grid;
+
+  constructor(
+    private usuarioService: UsuarioService,
+    private errorHandler: ErrorHandlerService,
+    private confirmation: ConfirmationService,
+    private messageService: MessageService,
+    private title: Title
+  ) { }
 
   ngOnInit() {
+    this.title.setTitle('Pesquisa de usuários');
+  }
+
+  pesquisar(pagina = 0) {
+    this.filtro.pagina = pagina;
+
+    this.usuarioService.pesquisar(this.filtro)
+      .then(resultado => {
+        this.totalRegistros = resultado.total;
+        this.usuarios = resultado.usuarios;
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  aoMudarPagina(event: LazyLoadEvent) {
+    const pagina = event.first / event.rows;
+    this.pesquisar(pagina);
+  }
+
+  confirmarExclusao(usuario: any) {
+    this.confirmation.confirm({
+      message: 'Tem certeza que deseja excluir?',
+      accept: () => {
+        this.excluir(usuario);
+      }
+    });
+  }
+
+  excluir(usuario: any) {
+    this.usuarioService.excluir(usuario.codigo)
+      .then(() => {
+        if (this.grid.first === 0) {
+          this.pesquisar();
+        } else {
+          this.grid.first = 0;
+        }
+
+        this.messageService.add({ severity: 'success', detail: 'Usuário excluído com sucesso!' });
+      })
+      .catch(erro => this.errorHandler.handle(erro));
   }
 
 }
--- wmmoney-ui/src/app/usuarios/usuarios.module.ts
+++ wmmoney-ui/src/app/usuarios/usuarios.module.ts
@@ -1,5 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { InputMaskModule } from 'primeng/inputmask';
+import { DataTableModule } from 'primeng/datatable';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { TooltipModule } from 'primeng/tooltip';
+import { PanelModule } from 'primeng/panel';
+import { DialogModule } from 'primeng/dialog';
 
 import { UsuariosPesquisaComponent } from './usuarios-pesquisa/usuarios-pesquisa.component';
 import { UsuariosRoutingModule } from './usuarios-routing.module';
@@ -8,6 +18,16 @@ import { SharedModule } from 'app/shared/shared.module';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+
+    InputTextModule,
+    ButtonModule,
+    DataTableModule,
+    TableModule,
+    TooltipModule,
+    InputMaskModule,
+    PanelModule,
+    DialogModule,
 
     SharedModule,
     UsuariosRoutingModule
